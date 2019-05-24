1faa6627c42097d1455ab9c93ead683249113c8d
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-05-25 06:10:58 +0800

    Update a2.sh

--- a2.sh
+++ a2.sh
@@ -8,6 +8,8 @@ if [ -d "$HOME/webui-aria2" ] ; then
 	cd ~/webui-aria2
 	nohup node node-server.js &
 	echo -e "\033[31maria2面板已在后台运行\033[0m"
+	echo -e "\033[31m请用chrome浏览器打开，localhost:8888\033[0m"
+	
 	cd ~
 	bash zs.sh
 else
