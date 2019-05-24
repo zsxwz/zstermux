269928926783dd6e02b508c3cdabd90265c065b0
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-05-24 10:16:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
-pkg upgrade
+pkg upgrade -y
 pkg install git -y
 git clone https://github.com/zsxwz/zstermux.git
 
