82e50b9f4e7447ad8a99803d8cd85d8876804ccb
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 11:53:29 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,7 +1,4 @@
-  #!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
   
- npm install - g http-server
- 
-	echo"已安装http服务"
-  echo""
-  sh http-server.sh
+npm install - g http-server
+
