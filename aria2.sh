d67d27c01aabfb6f8db44dabbf67c07f85e46124
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 17:10:09 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 echo ""
-echo "1.安装/运行aria2"
+echo "1.安装/运行aria2服务"
 echo "2.aria2使用推荐配置"
 echo "3.关闭aria2，使用推荐配置之前，需关闭服务。"
 echo ""
@@ -10,6 +10,7 @@ read int
 if
 	[ $int -eq "1" ] ; then
         sh a2.sh
+	sh aria2.sh
 elif
 	[ $int -eq "2" ] ; then
 	cd ~
