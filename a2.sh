bf203e514f0439492d24b8deb3e888d94a0de538
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 12:21:16 +0800

    Update a2.sh

--- a2.sh
+++ a2.sh
@@ -12,8 +12,7 @@ if [ -d "$HOME/webui-aria2" ] ; then
 	echo -e "\033[31maria2面板已在后台运行\033[0m"
 	echo -e "\033[31m请用chrome浏览器打开，localhost:8888\033[0m"
 	echo ""
-	
-	cd ~
+
 	bash aria2.sh
 else
 	cd ~
@@ -25,7 +24,7 @@ else
         echo -e "\033[31maria2面板已在后台运行\033[0m"
 	echo -e "\033[31m请用chrome浏览器打开，localhost:8888\033[0m"
 	echo ""
-	cd ~
+
 	bash aria2.sh
 fi
 exit
