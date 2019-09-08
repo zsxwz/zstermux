d40d1980b5ee78891068a8b5961fc67cdeac2026
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 09:28:24 +0800

    Add files via upload

---
+++ youtube.sh
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "1.安装Python，软件包比较大，运行环境没有预安装"
+echo "2.安装youtube-dl-interactive"
+echo -n "请输入需要执行的命令序号:"
+read int
+if
+	[ $int -eq "1" ] ; then
+        pkg install python -y
+	pip install upgrade pip
+	sh youtube.sh
+
+elif
+	[ $int -eq "2" ] ; then
+	pip install youtube-dl
+	npm install -g youtube-dl-interactive
+
+	echo "使用方法："
+	echo "youtube-dl-interactive 网页链接url"
+
+fi
