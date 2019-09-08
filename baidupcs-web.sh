bb86e942001efea1066d90e829f9ef18f4d94bb3
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 14:53:47 +0800

    Update baidupcs-web.sh

--- baidupcs-web.sh
+++ baidupcs-web.sh
@@ -4,8 +4,9 @@ if [ -d "$HOME/BaiduPCS-web" ] ; then
 	cd ~/BaiduPCS-web
 	screen -dmS baidu-web ./BaiduPCS-Go
 	echo ""
-	echo "31m如需修改配置请安装命令行版百度"
-	echo "baidupcs-web已在后台运行，请用浏览器打开访问，localhost:5299"
+	echo ""
+	echo -e "\033[31m如需修改配置请安装命令行版百度\033[0m"
+	echo -e "baidupcs-web已在后台运行，请用浏览器打开访问，localhost:5299"
 	echo ""
 	cd ~
 	sh zs.sh
