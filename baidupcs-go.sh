d70d14df9b2808ba0d601da644c66b0984464e43
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 09:41:24 +0800

    Update baidupcs-go.sh

--- baidupcs-go.sh
+++ baidupcs-go.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo ""
-echo -e "\033[31m输入，login，即可登录。或者 ,用bduss登录，login bduss=....，修改下载目录:cd /apps/baidu_shurufa\033[0m"
+echo -e "\033[31m输入，login，即可登录。或者 ,用bduss登录，login bduss=....\033[0m"
+echo -e "\033[31m修改下载目录:cd /apps/baidu_shurufa\033[0m"
 echo -e "\033[31m若下载无速度，修改目录之后，修改id：config set -appid=309847\033[0m"
 echo -e "\033[31m修改UA：config set -user_agent Opera/10.0a]\033[0m"
 echo ""
