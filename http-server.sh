458162f734d8c59d53eb16551258ffb05269a325
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 11:15:13 +0800

    Update http-server.sh

--- http-server.sh
+++ http-server.sh
@@ -1,10 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo ""
+echo "1.安装http-server"
+echo "2.运行http-server"
+echo "3.关闭http-server"
 
-if [ ! -d "$PREFIX/bin/http-server" ] ; then
-  screen -S http http-server
+echo -n "请输入需要执行的命令序号:"
+read int
+if
+	[ $int -eq "1" ] ; then
+        npm install - g http-server
+echo"已安装http服务"
+echo""
+sh http-server.sh
 
-else
-  npm install -g http-server
-  screen -S http http-serve
+elif
+	[ $int -eq "2" ] ; then
+	screen -S http http-server
+elif
+	[ $int -eq "3" ] ; then
+        screen -S http -X quit
+echo"服务已关闭"
+echo""
+sh http-server.sh
 fi
 exit
