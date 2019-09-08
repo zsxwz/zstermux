53568193d14337dc22b316c5d6eea38b0a6f243a
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 09:39:01 +0800

    Create baidupcs-web.sh

---
+++ baidupcs-web.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+if [ -d "$HOME/BaiduPCS-web" ] ; then
+	cd ~/BaiduPCS-web
+	screen -dmS baidu-web ./BaiduPCS-Go
+else
+	cd ~
+	wget -O 1.zip https://github.com/liuzhuoling2011/baidupcs-web/releases/download/3.6.8/BaiduPCS-Go-3.6.8-android-arm64.zip
+	unzip 1.zip && rm 1.zip
+	mv BaiduPCS-Go-3.6.8-android-arm64 BaiduPCS-web
+	cd ~/BaiduPCS-web
+	screen -dmS baidu-web ./BaiduPCS-Go
+fi
+echo ""
+echo -e "\033[31m如需修改配置请安装命令行版百度\033[0m"
+echo "baidupcs-web已在后台运行，请用浏览器打开访问，localhost:5299"
+echo ""
+sh zs.sh
+exit
