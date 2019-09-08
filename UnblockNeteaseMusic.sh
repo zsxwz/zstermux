087f6071018e696e35a8bb374bb9914152dfdee9
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 09:30:00 +0800

    Add files via upload

--- UnblockNeteaseMusic.sh
+++ UnblockNeteaseMusic.sh
@@ -2,7 +2,7 @@
 
 if [ -d "$HOME/UnblockNeteaseMusic" ] ; then
 	cd ~/UnblockNeteaseMusic
-	nohup node app.js &
+	screen -dmS music node app.js
         echo -e "\033[31m代理已在后台运行\033[0m"
 	echo -e "\033[31m请修改WiFi代理为，127.0.0.1，端口为，8080\033[0m"
 	cd ~
@@ -12,7 +12,7 @@ else
 	echo -e "\033正在下载安装\033[0m"
 	git clone https://github.com/nondanee/UnblockNeteaseMusic.git
 	cd ~/UnblockNeteaseMusic
-	nohup node app.js &
+	screen -dmS node app.js
 	echo -e "\033[31m代理已在后台运行\033[0m"
         echo -e "\033[31m请修改WiFi代理为，127.0.0.1，端口为，8080\033[0m"
 	cd ~
