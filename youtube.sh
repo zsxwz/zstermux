c146158070c0e5b6e9516a6821c06009736e1880
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-08 11:08:40 +0800

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo ""
 echo "1.安装Python，软件包比较大，运行环境没有预安装"
-echo "2.安装youtube-dl-interactive"
+echo "2.安装/运行youtube-dl-interactive"
 echo ""
 
 echo -n "请输入需要执行的命令序号:"
@@ -10,7 +10,6 @@ if
 	[ $int -eq "1" ] ; then
         pkg install python -y
 	pip install --upgrade pip
-	sh youtube.sh
 
 elif
 	[ $int -eq "2" ] ; then
