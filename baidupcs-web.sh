027218d0168480836be71723015d1680cb68a3ac
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-09-14 06:08:05 +0800

    Update baidupcs-web.sh

--- baidupcs-web.sh
+++ baidupcs-web.sh
@@ -9,9 +9,9 @@ if [ -d "$HOME/BaiduPCS-web" ] ; then
 	echo -e "\033[31m如需修改配置请安装命令行版百度\033[0m"
 	echo -e "baidupcs-web已在后台运行，请用浏览器打开访问，localhost:5299"
 	echo ""
-	cd ~
-	sh zs.sh
         am start -a android.intent.action.VIEW -d http://localhost:5299
+        cd ~
+	sh zs.sh
 else
 	cd ~
 	wget -O 1.zip https://github.com/liuzhuoling2011/baidupcs-web/releases/download/3.6.8/BaiduPCS-Go-3.6.8-android-arm64.zip
@@ -23,10 +23,8 @@ else
 	echo "如需修改配置请安装命令行版百度"
 	echo "baidupcs-web已在后台运行，请用浏览器打开访问，localhost:5299"
 	echo ""
-	
+        am start -a android.intent.action.VIEW -d http://localhost:5299
 	cd ~
 	sh zs.sh
-
-        am start -a android.intent.action.VIEW -d http://localhost:5299
 fi
 exit
