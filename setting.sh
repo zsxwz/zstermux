bae6b0360e2515fa196e56bfd384dbad21bda1e7
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 07:15:40 +0800

    Update and rename unzs.sh to setting.sh

---
+++ setting.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "1.oh-my-zsh终端配色"
+echo "2.恢复修改底部按键"
+echo "3.国内用户使用中科大软件源"
+echo "4.termux后台运行"
+
+read int
+if
+	[ $int -eq "1" ] ; then
+	cd ~
+	sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+	termux-reload-settings
+elif
+	[ $int -eq "2" ] ; then
+	sh key.sh
+
+elif
+	[ $int -eq "3" ] ; then
+	sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@'; $PREFIX/etc/apt/sources.list
+	apt update
+	apt upgrade
+elif
+	[ $int -eq "4" ] ; then
+	termux-wake-lock
+fi
+exit
