4b19d9160ba6fce1a7753680577a1bae2357738f
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 07:04:07 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -2,29 +2,20 @@
 
 echo ""
 echo "1.安装/运行aria2服务"
-echo "2.aria2使用推荐配置"
-echo "3.关闭aria2，使用推荐配置之前，需关闭服务。"
-echo ""
-echo -n "请输入需要执行的命令序号:"
+echo "2.修改aria2配置"
+
 read int
 if
 	[ $int -eq "1" ] ; then
-        sh a2.sh
-elif
-	[ $int -eq "2" ] ; then
 	cd ~
 	mkdir -p .config/aria2
 	cd zstermux
 	cp aria2.conf ~/.config/aria2/aria2.conf
-  echo ""
-	echo "已使用推荐配置，如需修改配置：vim .config/aria2/aria2.conf"
-  echo ""
-	sh aria2.sh
+        sh a2.sh
 elif
-	[ $int -eq "3" ] ; then
-	screen -S aria2 -X quit
-	echo ""
-	echo "aria2服务已关闭"
-	echo ""
+	[ $int -eq "2" ] ; then
+	vim .config/aria2/aria2.conf
+        screen -S aria2 -X quit
+        echo "服务已关闭，请重新安装运行"	
 	sh aria2.sh
 fi
