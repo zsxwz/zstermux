f00cbd44082d8a9af60a65960c7ebec14ef3d602
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 06:38:47 +0800

    Update youtube-dl.sh

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -3,14 +3,19 @@
 if [ -d "$PREFIX/bin/youtube-dl" ] ; then
   echo ""
 	echo "使用方法："
-	echo "youtube-dl-interactive 网页链接url"
+	echo "dl 视频链接url"
 else
+        echo "安装更新python"
+        pkg install python -y
+	pip install --upgrade pip
+	echo "安装ffmpeg解码"
         pkg install ffmpeg -y
+	echo "安装youtube-dl-interactive"
 	pip install youtube-dl
 	npm install -g youtube-dl-interactive
-  echo ""
+	alias dl='youtube-dl-interactive'
 	echo "使用方法："
-	echo "youtube-dl-interactive 网页链接url"
+	echo "dl 视频链接url"
 
 fi
 exit
