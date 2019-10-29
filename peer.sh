ee52a140eef6c66db878b503315c5a3635e80935
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 07:40:05 +0800

    Update peer.sh

--- peer.sh
+++ peer.sh
@@ -6,10 +6,11 @@ if [ -d "$HOME/.config/peerflix-server" ] ; then
         am start -a android.intent.action.VIEW -d http://localhost:9000
 
 else
-	pkg install nodejs -y
 	echo "安装peerflix-server"
+	cd ~
 	npm install -g peerflix-server
 	mkdir .config/peerflix-server
+	cd zstermux
 	cp config.json ~/.config/peerflix-server/config.json
 	screen -dmS peerflix-server peerflix-server
 	echo -e "\033[31m请用浏览器打开：http://localhost:5299\033[0m"
