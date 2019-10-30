70edcee7e1d7c77eecda7438c0897cb9faecc62d
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 12:02:23 +0800

    Create trackers-list-aria2.sh

---
+++ trackers-list-aria2.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+screen -S aria2 -X quit
+echo "aria2服务已关闭"
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+sed -i "s@bt-tracker.*@bt-tracker=$list@g" /.config/aria2.conf
+echo "tracker已更新，请重新运行aria2"
+cd ~
+sh zs.sh
