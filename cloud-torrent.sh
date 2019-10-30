b1dbbd7b669f3723b6f1b9d174622957e4e8ae2c
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 08:00:34 +0800

    Update cloud-torrent.sh

--- cloud-torrent.sh
+++ cloud-torrent.sh
@@ -1,20 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo ""
-echo -e "\033[31m输入，login，即可登录。或者 ,用bduss登录，login bduss=....\033[0m"
-echo -e "\033[31m修改下载目录:cd /apps/baidu_shurufa\033[0m"
-echo -e"\033[31m若下载无速度，修改目录之后，修改id：config set -appid=309847\033[0m"
-echo -e "\033[31m修改UA：config set -user_agent Opera/10.0a\033[0m"
-echo ""
 
-if [ -d "$HOME/BaiduPCS-Go" ] ; then
-	cd ~/BaiduPCS-Go
-	./BaiduPCS-Go
+if [ -d "$HOME/go/bin/cloud-torrent" ] ; then
+        cd ~
+	cd go/bin
+	./cloud-torrent -p 1024
+	am start -a android.intent.action.VIEW -d http://localhost:1024
 else
 	cd ~
-	wget -O 1.zip https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.6/BaiduPCS-Go-v3.5.6-android-21-arm64.zip
-	unzip 1.zip && rm 1.zip
-	mv BaiduPCS-Go-v3.5.6-android-21-arm64 BaiduPCS-Go
-	cd ~/BaiduPCS-Go
-	./BaiduPCS-Go
+	echo "安装golang"
+	echo "安装过程如果报错，请到终端设置，修改为中科大源"
+	pkg install golang -y
+	echo "安装cloud-torrent，请耐心等待"
+	go get -v github.com/jpillora/cloud-torrent
+	echo "cloud-torrent已在后台运行，请用浏览器打开访问，localhost:1024"
+	echo ""
+        am start -a android.intent.action.VIEW -d http://localhost:1024
 fi
 exit
