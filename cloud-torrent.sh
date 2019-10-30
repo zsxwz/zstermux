61a25861bbf1a0ac9597c76f1c29562865cf7e56
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 09:18:03 +0800

    Update cloud-torrent.sh

--- cloud-torrent.sh
+++ cloud-torrent.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo ""
 
-if [ -d "$HOME/go/bin/cloud-torrent" ] ; then
+if [ -x "$(command -v cloud-torrent)" ] ; then
         cd ~
 	cd go/bin
 	./cloud-torrent -p 1024
