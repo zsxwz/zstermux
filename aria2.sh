87cffa6b1c44662ee28ebca5bc6d155b31d07a2f
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 12:21:09 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,10 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo ""
 echo "1.安装/运行aria2服务"
-echo "2.更新tracker"
-echo -n "请输入要执行命令的序号"
+echo "2.安装aria2服务后更新tracker"
 echo ""
+echo -n "请输入需要执行的命令序号:"
 read int
 if
 	[ $int -eq "1" ] ; then
