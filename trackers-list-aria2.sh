148a92596e65d618ac3320f9cded971d3181bd9d
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 12:08:57 +0800

    Update trackers-list-aria2.sh

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+cd ~
 screen -S aria2 -X quit
 echo "aria2服务已关闭"
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
