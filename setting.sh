d751b9a147f8416f18bdc3daee8f957aa1b651df
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 10:01:19 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -18,8 +18,8 @@ elif
 
 elif
 	[ $int -eq "3" ] ; then
-	cd ~
-	sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@'; $PREFIX/etc/apt/sources.list
+	mv $PREFIX/etc/apt/sources.list $PREFIX/etc/apt/sources.list.bak
+	mv sources.list $PREFIX/etc/apt/sources.list
 	apt update
 	apt upgrade
 	sh zs.sh
