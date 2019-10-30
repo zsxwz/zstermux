9ee7f720f323833ca58a629e641f10ddde4ddb8e
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 10:07:34 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -2,8 +2,8 @@
 
 echo "1.oh-my-zsh终端配色"
 echo "2.恢复/修改底部按键"
-echo "3.国内用户使用中科大软件源"
-echo "4.termux后台运行"
+# echo "3.国内用户使用中科大软件源"
+echo "3.termux后台运行"
 echo ""
 echo -n "请输入需要执行的命令序号:"
 
@@ -17,15 +17,15 @@ elif
 	[ $int -eq "2" ] ; then
 	sh key.sh
 
+#elif
+	#[ $int -eq "3" ] ; then
+	#mv $PREFIX/etc/apt/sources.list $PREFIX/etc/apt/sources.list.bak
+	#mv sources.list $PREFIX/etc/apt/sources.list
+	#apt update
+	#apt upgrade
+	#sh zs.sh
 elif
 	[ $int -eq "3" ] ; then
-	mv $PREFIX/etc/apt/sources.list $PREFIX/etc/apt/sources.list.bak
-	mv sources.list $PREFIX/etc/apt/sources.list
-	apt update
-	apt upgrade
-	sh zs.sh
-elif
-	[ $int -eq "4" ] ; then
 	termux-wake-lock
 	sh zs.sh
 fi
