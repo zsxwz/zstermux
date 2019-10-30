c4273bc1c009decebe37da5e1ed7c206ba347b01
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 12:16:06 +0800

    Update trackers-list-aria2.sh

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -6,4 +6,5 @@ echo "aria2服务已关闭"
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 sed -i "s@bt-tracker.*@bt-tracker=$list@g" $HOME/.config/aria2/aria2.conf
 echo "tracker已更新，请重新运行aria2"
+cd zstermux
 sh aria2.sh
