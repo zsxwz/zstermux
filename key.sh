43f46521a84db1796f8673910a87b7c81274394a
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 09:32:27 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-
+echo "1.恢复更多按键"
+echo "2.修改按键"
 echo "修改按键,输入i，编辑。按esc退出编辑，输入:wq!，保存退出。"
+echo -n "请输入需要执行的命令序号:"
 read int
 if
 	[ $int -eq "1" ] ; then
