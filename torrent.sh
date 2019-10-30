91534d0bab8023814aaa50911f1d290595c06774
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 09:16:31 +0800

    Update torrent.sh

--- torrent.sh
+++ torrent.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "国内磁力种子下载，基本没有什么速度，请自备代理"
-echo "1.aria2高速下载"
-echo "2.peerflix-server磁力下载"
-echo "3.cloud-torrent磁力下载"
+echo "1.安装/运行aria2高速下载"
+echo "2.安装/运行peerflix-server磁力下载"
+echo "3.安装/运行cloud-torrent磁力下载"
 
 echo ""
 
