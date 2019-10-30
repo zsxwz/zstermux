89d0656cc7355e08b95dd747bc9014ed2012f713
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 09:25:23 +0800

    Update torrent.sh

--- torrent.sh
+++ torrent.sh
@@ -4,7 +4,7 @@ echo "1.安装/运行aria2高速下载"
 echo "2.安装/运行peerflix-server磁力下载"
 echo "3.安装/运行cloud-torrent磁力下载"
 echo ""
-echo "请输入需要执行的命令序号:"
+echo -n "请输入需要执行的命令序号:"
 read int
 if
 	[ $int -eq "1" ] ; then
