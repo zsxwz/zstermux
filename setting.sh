4f3739b32a567bc9c1ff9287669abd37960340a5
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 09:37:28 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -22,8 +22,10 @@ elif
 	sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@'; $PREFIX/etc/apt/sources.list
 	apt update
 	apt upgrade
+	sh zs.sh
 elif
 	[ $int -eq "4" ] ; then
 	termux-wake-lock
+	sh zs.sh
 fi
 exit
