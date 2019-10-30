c52a9ed49d40ebe558a60cbea0e5b469ea805026
zsxwz <34711528+zsxwz@users.noreply.github.com>
2019-10-30 08:07:19 +0800

    Update zs.sh

--- zs.sh
+++ zs.sh
@@ -7,15 +7,15 @@ echo ""
 echo -e "\033[31m第一次安装使用，或者安装过程报错，请务必安装一次运行环境,输入1，即可。\033[0m"
 echo -e "\033[31m国内用户推荐使用科大源，终端设置里修改即可。\033[0m"
 echo "1.基础运行环境，软件安装"
-echo "2.百度网盘提取码查询"
+echo "2.百度网盘提取码及不限速下载"
 echo "3.网易云音乐代理"
 echo "4.常用磁力下载工具"
-echo "5.百度网盘不限速"
-echo "6.youtube-dl视频下载"
-echo "7.http-server服务"
-echo "8.推荐终端设置"
+echo "5.youtube-dl视频下载"
+echo “6.http-server服务"
+echo "7.推荐终端设置"
+echo "0.退出脚本"
 echo ""
-echo -n "请输入需要执行的命令序号:"
+echo "请输入需要执行的命令序号:"
 read int
 if
 	[ $int -eq "1" ] ; then
@@ -26,7 +26,7 @@ if
 
 elif
 	[ $int -eq "2" ] ; then
-	bash baidupankey.sh
+	bash baidu.sh
 
 elif
 	[ $int -eq "3" ] ; then
@@ -38,23 +38,10 @@ elif
 
 elif
 	[ $int -eq "5" ] ; then
-	bash baidu.sh
-
-elif
-	[ $int -eq "6" ] ; then
 	sh youtube-dl.sh
 elif
         [ $int -eq "7" ] ; then
 	bash http-server.sh
-
-elif
-	[ $int -eq "8" ] ; then
-	cd ~
-	sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
-	termux-reload-settings
-elif
-	[ $int -eq "9" ] ; then
-	sh key.sh
 
 elif
 	[ $int -eq "0" ] ; then
