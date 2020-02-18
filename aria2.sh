7bcf68d73d1deb8e2c43dd5630d2e0357bac6220
zsxwz <34711528+zsxwz@users.noreply.github.com>
2020-02-18 16:55:29 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 echo "1.安装/运行aria2服务"
-echo "2.安装aria2服务后更新tracker"
+echo "2.设置自动更新tracker"
 echo ""
 echo -n "请输入需要执行的命令序号:"
 read int
@@ -10,5 +10,5 @@ if
         sh a2.sh
 elif
 	[ $int -eq "2" ] ; then
-	sh trackers-list-aria2.sh
+	crontab -l | { cat; echo "30 19 * * * $HOME/zstermux/trackers-list-aria2.sh"; } | crontab -
 fi
