53aeda025013c93510373b95af65ca1bd3779bf7
zsxwz <34711528+zsxwz@users.noreply.github.com>
2020-03-05 17:27:49 +0800

    Update cloud-torrent.sh

--- cloud-torrent.sh
+++ cloud-torrent.sh
@@ -2,7 +2,7 @@
 
 if [ ! -x "$HOME/cloud-torrent" ] ; then
         cd ~
-	screen -dmS cloud-torrert ./cloud-torrent -p 1024
+	screen -dmS cloud-torrert cloud-torrent -p 1024
 	echo "cloud-torrent已在后台运行，请用浏览器打开访问，localhost:1024"
 	am start -a android.intent.action.VIEW -d http://localhost:1024
 	cd ~
@@ -11,9 +11,9 @@ else
 	cd ~
 	wget https://github.com/boypt/simple-torrent/releases/download/1.2.6/cloud-torrent_linux_amd64.gz
 	gzip -d cloud-torrent_linux_amd64.gz
-	mv cloud-torrent_linux_amd64 cloud-torrent
-	chmod +x cloud-torrent
-	screen -dmS cloud-torrert ./cloud-torrent -p 1024
+	chmod +x cloud-torrent_linux_amd64
+	mv cloud-torrent_linux_amd64 /data/data/com.termux/files/usr/bin/cloud-torrent
+	screen -dmS cloud-torrert cloud-torrent -p 1024
 	echo "cloud-torrent已在后台运行，请用浏览器打开访问，localhost:1024"
 	echo ""
         am start -a android.intent.action.VIEW -d http://localhost:1024
