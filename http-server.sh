cab623a5845db0b9ce1a2ca8dd2a6988e071623a
zsxwz <34711528+zsxwz@users.noreply.github.com>
2020-03-05 17:19:21 +0800

    Update http-server.sh

--- http-server.sh
+++ http-server.sh
@@ -13,7 +13,7 @@ else
 	unzip chfs-linux-arm64-2.0.zip
 	chmod +x chfs
 	mv chfs /data/data/com.termux/files/usr/bin/chfs
-	screen -dmS chfs ./chfs --port=1234 --path="/sdcard"
+	screen -dmS chfs chfs --port=1234 --path="/sdcard"
 	echo "cloud-torrent已在后台运行，请用浏览器打开访问，localhost:1234"
 	am start -a android.intent.action.VIEW -d http://localhost:1234
 	cd ~
