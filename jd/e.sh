8ea3a71bb3c01cf48fcf04f446b1fb1379cada2d
zsxwz <34711528+zsxwz@users.noreply.github.com>
2021-05-06 07:11:11 +0800

    Create e.sh

---
+++ jd/e.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+        cd /data/data/com.termux/files/home/jd_scripts
+        set -o allexport
+        source env1
+        source all
+        source resolve.sh
